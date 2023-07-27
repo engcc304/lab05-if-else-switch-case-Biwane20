ad4cf24b6541ce72a7f13780c84e9716c752ca82
Biwane20 <biwbew20@gmail.com>
2023-07-27 14:44:55 +0700

    Lap ud New

--- Lab/Lab05-If-Else-Switch-Case.cpp
+++ Lab/Lab05-If-Else-Switch-Case.cpp
@@ -37,9 +37,6 @@
 
 int main()
 {
-
-#include <stdio.h>
-
     float score;
 
     printf(" Enter score : ");
@@ -49,27 +46,27 @@ int main()
     {
         printf("A !");
     }
-    else if (score >= 75)
+    else if (score >= 75 && score <= 79)
     {
         printf("B+ !");
     }
-    else if (score >= 70)
+    else if (score >= 70 && score <= 74)
     {
         printf("B !");
     }
-    else if (score >= 65)
+    else if (score >= 65 && score <= 69)
     {
         printf("C+ !");
     }
-    else if (score >= 60)
+    else if (score >= 60 && score <= 64)
     {
         printf("C !");
     }
-    else if (score >= 55)
+    else if (score >= 55 && score <= 59)
     {
         printf("D+ !");
     }
-    else if (score >= 50)
+    else if (score >= 50 && score <= 54)
     {
         printf("D !");
     }
